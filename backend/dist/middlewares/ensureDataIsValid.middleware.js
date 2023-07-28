8ded8154aa822370408ac374946b9cb46e6ac5cb
Felipe Domingues <110036081+mongemingau@users.noreply.github.com>
2023-07-28 14:12:41 -0300

    Delete backend/dist directory

--- backend/dist/middlewares/ensureDataIsValid.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ensureDataIsValid = void 0;
-const ensureDataIsValid = (schema) => (req, res, next) => {
-    const validateDAta = schema.parse(req.body);
-    req.body = validateDAta;
-    return next();
-};
-exports.ensureDataIsValid = ensureDataIsValid;
