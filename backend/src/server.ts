14827cdb1bea5d109191c95ee4cb3237bfca3695
felipe <felipe@hcdesenvolvimento.com.br>
2023-07-29 20:45:58 -0300

    render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,10 @@ const cors = require('cors');
 
 AppDataSource.initialize()
     .then(() => {
-        app.use(cors())
+        app.use(cors({
+            origin: 'http://localhost:5173',
+            credentials: true, 
+          }));
         const PORT = process.env.PORT || 3000
         app.listen(PORT, () => {
             console.log(`Server is running on ${PORT}`)
