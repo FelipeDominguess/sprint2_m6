c66d849e58a29d60b84b4f425f708aed6cda81b7
felipe <felipe@hcdesenvolvimento.com.br>
2023-07-29 20:19:06 -0300

    render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,13 @@ import app from './app'
 import "dotenv/config"
 import { AppDataSource } from "./data-source"
 
+const cors = require('cors');
+
+
 
 AppDataSource.initialize()
     .then(() => {
+        app.use(cors())
         const PORT = process.env.PORT || 3000
         app.listen(PORT, () => {
             console.log(`Server is running on ${PORT}`)
